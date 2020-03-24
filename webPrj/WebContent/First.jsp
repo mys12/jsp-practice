0fab671ea7e2f684ba07bdb4d00c229f732d1e73
mys12 <mys62811@naver.com>
2020-03-24 16:47:11 +0900

    day01_first practice

---
+++ webPrj/WebContent/First.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>첫 예제</title>
+</head>
+<body>
+<%
+int a = (int)(Math.random()*100+2);
+int sum = 0;
+for(int i = 1; i < a; i++){
+sum += i;
+}%>
+1+2+3+...<%=a%>까지의 합 : <%=sum%>
+</body>
+</html>
---
+++ webPrj/src/FirstServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/First.do")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int a = (int)(Math.random()*100+2);
+		int sum = 0;
+		for(int i = 1; i < a; i++){
+			sum += i;
+		}
+		System.out.printf("1+2+3+...+%d의 합 : %d", a,sum);
+		System.out.println("");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
