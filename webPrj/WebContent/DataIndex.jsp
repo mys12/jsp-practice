f7a5986df222b1fd43929f62ad734dd88aba2ec3
mys12 <mys62811@naver.com>
2020-03-30 16:42:31 +0900

    day06_practice session

--- webPrj/WebContent/DataIndex.jsp
+++ webPrj/WebContent/DataIndex.jsp
@@ -7,10 +7,17 @@
 <title>메인 페이지</title>
 </head>
 <body>
+<%if(session.getAttribute("userid")!=null){ %>
 1. <a href="/DataInput.jsp"><input type=button value=정보입력></a>
 <br><br>
 2. <a href="/Data.do?action=list"><input type=button value=목록조회></a>
 <br><br>
 3. <a href="/DataSearch.jsp"><input type=button value=정보검색></a>
+<br><br>
+<a href="/Login.do"><input type=button value=로그아웃></a>
+
+<% } else { response.sendRedirect("/Login.jsp"); }%>
+
+
 </body>
 </html>
---
+++ webPrj/src/LoginServlet.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		HttpSession session = request.getSession();
+		session.invalidate();
+		request.setAttribute("message", "로그아웃 되었습니다.");
+		request.getRequestDispatcher("/Login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id= request.getParameter("id");
+		String pw =request.getParameter("pw");
+		String url = "";
+		if(id.equals("mys") && pw.equals("0000")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("userid", id);
+			response.sendRedirect("/DataIndex.jsp");
+			return;
+		}else {
+			request.setAttribute("message", "아이디 또는 비밀번호가 다릅니다.");
+			url = "/Login.jsp";
+		}
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+}
